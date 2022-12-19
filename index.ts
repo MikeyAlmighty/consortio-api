0f38a24adac4ca0e84493bc39d32ce2a333224f9
Mikey <45830554+MikeyAlmighty@users.noreply.github.com>
2022-12-19 15:13:34 +0200

    testing cors allow all

--- index.ts
+++ index.ts
@@ -14,9 +14,9 @@ app.use(cors())
 app.options('*', cors())
 
 app.all('*', function(req, res, next) {
-       res.header("Access-Control-Allow-Origin", "*");
-       res.header("Access-Control-Allow-Headers", "X-Requested-With");
-       res.header('Access-Control-Allow-Headers', 'Content-Type');
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
        next();
 });
 
