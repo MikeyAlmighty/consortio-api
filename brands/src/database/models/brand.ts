70dcba5e13d50bb6fc81c15f3a253a149fe05777
MikeyAlmighty <mike.leibbrandt@gmail.com>
2022-12-23 12:28:37 +0200

    WIP: Restructured into microservices

---
+++ brands/src/database/models/brand.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const brandSchema = new Schema({
+    name: {
+        type: String
+    },
+    origin: {
+        type: String
+    },
+    active: {
+        type: Boolean
+    },
+    inceptionDate: {
+        type: Date
+    },
+    IPR: {
+      type: String,
+      enum : ['Patent','Trade secret', 'Trademark', 'Copyright', 'Invention'],
+      default: 'Patent'
+    }
+}, { timestamps: true })
+
+export default mongoose.model('brand', brandSchema)
